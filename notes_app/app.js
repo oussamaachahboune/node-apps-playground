fb769a2b0d56f18837f5f037d96906127cefa583
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-05 06:02:14 +0000

    feat: add yargs CLI with add, remove, list, and read commands

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,7 +1,39 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-const msg = getNotes();
-console.log(msg);
+yargs.version("1.1.0");
 
-console.log(chalk.green.inverse.bold("Success!"));
+yargs.command({
+  command: "add",
+  describe: "Add a new note to your notes list",
+  handler: function () {
+    console.log(chalk.green("New note added successfully!"));
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove an existing note by its title",
+  handler: function () {
+    console.log(chalk.red("Note removed successfully!"));
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "List all of your saved notes",
+  handler: function () {
+    console.log(chalk.blue("Fetching all your notes..."));
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read the contents of a specific note by title",
+  handler: function () {
+    console.log(chalk.yellow("Reading note..."));
+  },
+});
+
+console.log(yargs.argv);
