b38c530a62de4e1017186ab430ecb45b5994d2d1
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-07 03:15:52 +0000

    feat: add error handling for weather and geocoding requests
    - handle network errors for both weatherstack and Mapbox API calls
    - handle invalid location response from weatherstack
    - handle empty features array from Mapbox geocoding response

--- weather_app/app.js
+++ weather_app/app.js
@@ -7,20 +7,32 @@ const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHE
 
 request({ url: url, json: true }, (error, response) => {
   // automatically parses response as JSON
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      ". It is currently " +
-      response.body.current.temperature +
-      " degress out. It feels like " +
-      response.body.current.feelslike +
-      " degress out.",
-  );
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find location.");
+  } else {
+    console.log(
+      response.body.current.weather_descriptions[0] +
+        ". It is currently " +
+        response.body.current.temperature +
+        " degress out. It feels like " +
+        response.body.current.feelslike +
+        " degress out.",
+    );
+  }
 });
 
 const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API_KEY}&limit=1`;
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log("Unable to connect to location service!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
