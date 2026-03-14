8488731475f02fabc978f92d31676cdd7c01ceee
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-14 00:33:34 +0000

    refactor: apply ES6 destructuring across callbacks and utilities
    - Destructure `{ latitude, longitude, location }` directly in geocode callback in app.js
    - Destructure `{ body }` from response in forecast.js and geocode.js
    - Use property shorthand for `url` in request options
    - Add ES6 object destructuring playground examples

---
+++ playground/4_es6_objects.js
@@ -0,0 +1,38 @@
+// property shorthand
+
+const name = "Oussama";
+const userAge = 23;
+
+const user = {
+  name, // name: name,
+  age: userAge,
+  location: "Agadir",
+};
+
+console.log(user);
+
+// object destructuring
+
+const product = {
+  label: "Red notebook",
+  price: 3,
+  stock: 201,
+  salePrice: undefined,
+};
+
+// const label = product.label;
+// const stock = product.stock;
+
+const { label: productLabel, stock, rating = 5 } = product;
+
+// console.log(productLabel);
+// console.log(stock);
+// console.log(rating);
+
+const transaction = (type, { label, stock }) => {
+  //   const { label, stock } = myProduct;
+
+  console.log(type, label, stock);
+};
+
+transaction("order", product);
--- weather_app/app.js
+++ weather_app/app.js
@@ -6,17 +6,17 @@ const address = process.argv[2];
 if (!address) {
   console.log("Please provide an address.");
 } else {
-  geocode(address, (error, data) => {
+  geocode(address, (error, { latitude, longitude, location }) => {
     if (error) {
       return console.log(error);
     }
 
-    forecast(data.latitude, data.longitude, (error, weatherData) => {
+    forecast(latitude, longitude, (error, weatherData) => {
       if (error) {
         return console.log(error);
       }
 
-      console.log(data.location);
+      console.log(location);
       console.log(weatherData);
     });
   });
