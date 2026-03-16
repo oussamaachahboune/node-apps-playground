234f339e6c7a77d67cbfc0f44010c7473014107d
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-16 22:19:08 +0000

    feat: integrate Handlebars templating engine with dynamic views
    - Add `hbs` as a view engine dependency
    - Convert static HTML files to `.hbs` templates in `views/`
    - Render dynamic data (title, name, helpText) via route handlers
    - Configure `app.set("view engine", "hbs")` in Express app

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -5,8 +5,35 @@ const app = express();
 const port = 3000;
 publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Oussama Achahboune",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Oussama Achahboune",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "Mostly cloudy",
+    location: "Agadir",
+  });
+});
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
