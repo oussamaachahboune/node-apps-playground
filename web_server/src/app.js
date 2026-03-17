1d7026f18f17619cc41d8e4cb524e9102c5b9159
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-17 04:31:41 +0000

    feat: add HBS partials and reorganize templates structure
    - Create `header.hbs` and `footer.hbs` partials with nav links and author info
    - Move view templates into `templates/views/` subdirectory
    - Register partials path using `hbs.registerPartials(partialsPath)`
    - Replace inline `<h1>` and author `<p>` tags with `{{>header}}` and `{{>footer}}`
    - Add `title` and `name` to help route render data

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,16 +1,19 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 const port = 3000;
 
 // Define paths for Express config
 publicDirectoryPath = path.join(__dirname, "../public");
-viewsPath = path.join(__dirname, "../templates");
+viewsPath = path.join(__dirname, "../templates/views");
+partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -32,6 +35,8 @@ app.get("/about", (req, res) => {
 app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Oussama Achahboune",
   });
 });
 
