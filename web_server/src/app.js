af660ce379aa7d29fdbb55d3b55ba5a2de8b3edd
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-17 01:24:32 +0000

    refactor: move views to custom templates directory and configure path
    - Rename `views/` to `templates/` for all `.hbs` files
    - Set custom views path using `app.set("views", viewsPath)`
    - Add comments to clarify Express config sections

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -3,9 +3,16 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
-publicDirectoryPath = path.join(__dirname, "../public");
 
+// Define paths for Express config
+publicDirectoryPath = path.join(__dirname, "../public");
+viewsPath = path.join(__dirname, "../templates");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
