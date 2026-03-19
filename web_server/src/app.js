dd362a29dbaf95afd2a42db765113d64ea213652
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-19 02:31:07 +0000

    feat: add query param validation and address handling to /weather route
    - Return error if `address` query param is missing
    - Include `address` in the response payload
    - Add commented-out `/products` route as reference example

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -41,11 +41,29 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
   res.send({
     forecast: "Mostly cloudy",
     location: "Agadir",
+    address: req.query.address,
   });
 });
+
+// app.get("/products", (req, res) => {
+//   if (!req.query.search) {
+//     return res.send({ error: "You must provide a search term" });
+//   }
+
+//   console.log(req.query.search);
+//   res.send({
+//     products: [],
+//   });
+// });
 
 app.get("/help/*splat", (req, res) => {
   res.render("404", {
