ba048676c3e87f4a96219ed48184aba6b5ae0065
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-21 08:42:58 +0000

    feat: add client-side fetch call to /weather endpoint
    - Fetch weather data for hardcoded address `agadir` from local API
    - Log `location` and `forecast` on success, or `error` on failure
    - Include commented-out JSONPlaceholder example for reference

--- web_server/public/js/app.js
+++ web_server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log("Client side JS file is loaded!");
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+fetch("http://localhost:3000/weather?address=agadir").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
