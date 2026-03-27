9a0e331e1bf921eba0bb50008c3f1ddd37ae42e9
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-27 12:31:28 +0100

    feat: add GET endpoints for tasks collection and individual task
    - Add GET /tasks to fetch all tasks
    - Add GET /tasks/:id to fetch task by ID with 404 handling
    - Fix User.findById call to pass id directly instead of as object

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -34,7 +34,7 @@ app.get("/users", (req, res) => {
 app.get("/users/:id", (req, res) => {
   const _id = req.params.id;
 
-  User.findById({ _id })
+  User.findById(_id)
     .then((user) => {
       if (!user) {
         return res.status(404).send();
@@ -57,6 +57,32 @@ app.post("/tasks", (req, res) => {
     })
     .catch((e) => {
       res.status(400).send(e);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
     });
 });
 
