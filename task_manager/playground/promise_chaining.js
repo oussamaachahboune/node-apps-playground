0558d54076681ce07f76a136d44d4f214d25379d
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-28 11:38:00 +0100

    refactor: replace promise chaining with async/await in playground files
    - Wrap User update and Task delete logic in async functions
    - Comment out original `.then`-based implementations for reference

--- task_manager/playground/promise_chaining.js
+++ task_manager/playground/promise_chaining.js
@@ -1,13 +1,28 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("69c524938736258c1bbc93ef", { age: 1 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
-  .then((result) => {
-    console.log(result);
+// User.findByIdAndUpdate("69c524938736258c1bbc93ef", { age: 1 })
+//   .then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+
+  return count;
+};
+
+updateAgeAndCount("69c524938736258c1bbc93ef", 2)
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
