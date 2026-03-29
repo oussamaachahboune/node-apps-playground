e95c36a362081c1b79ad35d7eab45a520469aee9
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-03-29 21:37:21 +0100

    feat: add user login endpoint and credential validation
    - Add `findByCredentials` static method for email/password auth
    - Add `unique` constraint to user email field
    - Add `POST /users/login` route using credential validation
    - Remove leftover bcrypt test code from index.js

--- task_manager/src/models/user.js
+++ task_manager/src/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -37,6 +38,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre("save", async function () {
   const user = this;
 
