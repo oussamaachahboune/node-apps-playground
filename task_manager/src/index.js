d58bb1048e1aabd0394aa48daaa7e688b4cd9241
Oussama ACHAHBOUNE <156251219+oussamaachahboune@users.noreply.github.com>
2026-03-31 10:25:25 +0100

    feat/api-auth-security (#8)

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Site is currently down. Check back soon!");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -13,3 +25,18 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+// const Task = require("./models/task");
+// const User = require("./models/user");
+
+// const main = async () => {
+//   const task = await Task.findById("69cac104544926fac3e8c83d");
+//   await task.populate("owner");
+//   console.log(task.owner);
+
+//   const user = await User.findById("69cab39a8ca717c515763531");
+//   await user.populate("tasks");
+//   console.log(user.tasks);
+// };
+
+// main();
--- task_manager/src/models/user.js
+++ task_manager/src/models/user.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -34,6 +38,81 @@ const User = mongoose.model("User", {
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = await jwt.sign(
+    { _id: user._id.toString() },
+    "thisismynewcourse",
+  );
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre("save", async function () {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  // next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function () {
+    await Task.deleteMany({ owner: this._id });
+  },
+);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
