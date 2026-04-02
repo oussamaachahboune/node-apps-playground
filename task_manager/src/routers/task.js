6d2bb0f8475c74cdcf00fd31952fd35a0e19b078
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-04-02 10:37:10 +0100

    feat: add pagination support to GET /tasks endpoint
    - Support `?limit=10&skip=0` query params for paginating task results
    - Pass `options` with `limit` and `skip` to `populate`

--- task_manager/src/routers/task.js
+++ task_manager/src/routers/task.js
@@ -19,6 +19,7 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
 
@@ -32,6 +33,10 @@ router.get("/tasks", auth, async (req, res) => {
     await req.user.populate({
       path: "tasks",
       match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+      },
     });
     res.send(req.user.tasks);
   } catch (e) {
