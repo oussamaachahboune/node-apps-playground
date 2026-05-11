eea2f7fda3bff9aa57eacd928a72ca0393ce0496
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-05-11 17:28:33 +0100

    feat: store avatar uploads in database as binary buffer
    - Add `avatar` Buffer field to user schema
    - Remove multer `dest` to keep file in memory instead of disk
    - Save uploaded file buffer to user document on avatar upload
    - Add auth middleware to avatar upload route
    - Add `DELETE /users/me/avatar` endpoint to remove avatar

--- task_manager/src/routers/user.js
+++ task_manager/src/routers/user.js
@@ -90,7 +90,6 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: "avatars",
   limits: {
     fileSize: 1000000,
   },
@@ -105,13 +104,22 @@ const upload = multer({
 
 router.post(
   "/users/me/avatar",
+  auth,
   upload.single("avatar"),
-  (req, res) => {
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message });
   },
 );
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
 
 module.exports = router;
