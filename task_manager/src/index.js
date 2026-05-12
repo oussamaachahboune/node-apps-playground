6bf44e2ea73864f211368f488bb157748966ad08
Oussama ACHAHBOUNE <156251219+oussamaachahboune@users.noreply.github.com>
2026-05-12 01:59:50 +0100

    feat/file-uploads (#10)

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -6,6 +6,35 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// const multer = require("multer");
+
+// const upload = multer({
+//   dest: "images",
+//   limits: {
+//     fileSize: 1000000,
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(doc|docx)$/)) {
+//       return cb(new Error("Please upload a Word document"));
+//     }
+
+//     cb(undefined, true);
+//     // cb(undefined, false);
+//     // cb(new Error("File must be a PDF"));
+//   },
+// });
+
+// app.post(
+//   "/upload",
+//   upload.single("upload"),
+//   (req, res) => {
+//     res.send();
+//   },
+//   (error, req, res, next) => {
+//     res.status(400).send({ error: error.message });
+//   },
+// );
+
 // app.use((req, res, next) => {
 //   if (req.method === "GET") {
 //     res.send("GET requests are disabled");
