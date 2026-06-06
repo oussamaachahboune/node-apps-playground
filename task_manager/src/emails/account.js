9eca05fcbc8a7d14c2479df160c5d3db90056ddf
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-06-06 03:53:39 +0100

    feat: implement email functionality for user registration and cancellation

--- task_manager/src/emails/account.js
+++ task_manager/src/emails/account.js
@@ -3,9 +3,25 @@ require("dotenv").config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-resend.emails.send({
-  from: "oussama.achahboune.2002@gmail.com",
-  to: "oussama.achahboune.2002@gmail.com",
-  subject: "This is my first creation!",
-  html: "<p>I hope this one actually get to you.</p>",
-});
+const sendWelcomeEmail = (email, name) => {
+  resend.emails.send({
+    from: "",
+    to: email,
+    subject: "Thanks for joining in!",
+    html: `<p>Welcome to the app, ${name}. Let me know how you get along with the app.</p>`,
+  });
+};
+
+const sendCancelationEmail = (email, name) => {
+  resend.emails.send({
+    from: "",
+    to: email,
+    subject: "Sorry to see you go!",
+    html: `<p>Goodbye, ${name}. I hope to see you back sometime soon.</p>`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail,
+};
