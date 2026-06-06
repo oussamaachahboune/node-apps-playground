23a5055f0dd75ba3e3844c5911f11c0a9492d7b6
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-06-06 20:26:17 +0100

    feat: add jest for testing and create initial test file

---
+++ task_manager/tests/math.test.js
@@ -0,0 +1,16 @@
+test("hello world!", () => {});
+
+test("this should fail", () => {
+  throw new Error("Failure!");
+});
+
+//
+// Why test?
+//
+// - Saves time
+// - Creates reliable software
+// - Gives flexibility to developers
+//   - Refactoring
+//   - Collaborating
+//   - Profiling
+// - Peace of mind
