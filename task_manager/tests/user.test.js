898103b158f10e412556fadabe0221bba6dbb38c
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-06-10 10:43:08 +0100

    test: add login endpoint test cases
    - Add db cleanup before each test using supertest setup
    - Add test user creation before running login tests
    - Add successful and failed login test cases

--- task_manager/tests/user.test.js
+++ task_manager/tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  name: "Sami",
+  email: "sami@example.com",
+  password: "Red12345!",
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("should signup a new user", async () => {
   await request(app)
@@ -10,4 +22,24 @@ test("should signup a new user", async () => {
       password: "Red12345!",
     })
     .expect(201);
+});
+
+test("should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("should not login nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "Red12345@",
+    })
+    .expect(400);
 });
