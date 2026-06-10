1e83dcf95efca16964b9370ff093a39e84bc51fa
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-06-10 19:18:51 +0100

    test: clean up test setup and add task creation test
    - remove unused math tests
    - add --runInBand to test script
    - move shared user data and setup to fixtures
    - add test case for task creation

--- task_manager/tests/user.test.js
+++ task_manager/tests/user.test.js
@@ -1,26 +1,9 @@
 const request = require("supertest");
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 const app = require("../src/app");
 const User = require("../src/models/user");
+const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-  _id: userOneId,
-  name: "Sami",
-  email: "sami@example.com",
-  password: "Red12345!",
-  tokens: [
-    {
-      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
-    },
-  ],
-};
-
-beforeEach(async () => {
-  await User.deleteMany();
-  await new User(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test("should signup a new user", async () => {
   const response = await request(app)
