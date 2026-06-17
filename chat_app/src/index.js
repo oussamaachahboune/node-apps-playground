774d2c511c7bac271c6317246bf069e3c8332a8f
ACHAHBOUNE Oussama <oussama.achahboune.2002@gmail.com>
2026-06-17 14:58:27 +0100

    feat: implement real-time counter updates with event-based communication
    - server emits countUpdated events to all connected clients
    - client listens for countUpdated events and updates the displayed count
    - client emits a button click event to the server
    - server receives the event, updates the counter, and broadcasts the updated count to all connected clients

--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -12,9 +12,18 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+
 // Listen for new connections to Socket.io
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New WebSocket connection");
+
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+    io.emit("countUpdated", count);
+  });
 });
 
 server.listen(port, () => {
